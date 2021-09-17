d14979739f288216ac5d6eef48271c87ed5fd6dd
Natasha Che <natasha@natashache.com>
2021-09-17 13:20:35 -0400

    verifying contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,8 @@
 */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-const { API_URL, PRIVATE_KEY } = process.env;
+require("@nomiclabs/hardhat-etherscan");
+const { API_URL, PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
 module.exports = {
    solidity: "0.8.0",
    // defaultNetwork: "hardhat",
@@ -18,4 +19,7 @@ module.exports = {
       //    accounts: [`0x${PRIVATE_KEY}`]
       // }
    },
+  etherscan: {
+    apiKey: ETHERSCAN_KEY,
+  }
 }
